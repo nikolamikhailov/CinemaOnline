cf3f326efe54b3e9b1b4380b4e5ffdc2ca56b26a
l4gunner4l <mix.nik155@gmail.com>
2020-10-03 12:27:14 +0300

    Initial commit

---
+++ app/src/main/java/ru/l4gunner4l/cinemaonline/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.l4gunner4l.cinemaonline
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
